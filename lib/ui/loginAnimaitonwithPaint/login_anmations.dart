1575583761320480a65584fb01fbb5c6e313c4c8
tushardubey <dubey.tushar23@gmail.com>
2021-01-08 17:07:05 +0530

    animation added

--- lib/ui/loginAnimaitonwithPaint/login_anmations.dart
+++ lib/ui/loginAnimaitonwithPaint/login_anmations.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demos/ui/loginAnimaitonwithPaint/bg_painter.dart';
+import 'package:flutter_demos/ui/loginAnimaitonwithPaint/widgets/background_painter.dart';
 
 class LoginAnimations extends StatefulWidget {
   @override
   _LoginAnimationsState createState() => _LoginAnimationsState();
 }
 //https://www.youtube.com/watch?v=bpvpbQF-2Js&ab_channel=FunwithFlutter
-class _LoginAnimationsState extends State<LoginAnimations> {
+class _LoginAnimationsState extends State<LoginAnimations> with TickerProviderStateMixin{
+AnimationController _controller;
+  @override
+  void initState() {
+    _controller=AnimationController(vsync: this,duration: Duration(seconds: 2));
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +21,11 @@ class _LoginAnimationsState extends State<LoginAnimations> {
         children: [
           SizedBox.expand(
             child: CustomPaint(
-              painter: BgPainter(),
+              painter: BackgroundPainter(animation: _controller.view
+              ),
             ),
-          )
+          ),
+          Center(child: RaisedButton(onPressed: ()=>_controller.forward(from: 0),child: Text('Animate'),))
         ],
       ),
     );
